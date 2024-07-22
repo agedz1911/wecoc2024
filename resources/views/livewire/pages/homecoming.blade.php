49329a2a30a84ca3c111f68855b232d589eb390b
unknown <asep.cgeds@gmail.com>
2024-07-22 20:56:42 +0700

    fix:homecoming

--- resources/views/livewire/pages/homecoming.blade.php
+++ resources/views/livewire/pages/homecoming.blade.php
@@ -1,13 +1,13 @@
 <div class="bg-slate-50">
     <div class="bg-local pt-20 pb-52 lg:px-20 px-5 bg-home-coming">
         <div class="flex-col flex gap-3 mb-16">
-            <h1 class="text-4xl text-primary-600 font-semibold">Homecoming Day</h1>
-            <p>Alumni Gathering Event</p>
-            <p>Department of Cardiology and Vascular Medicine FKUI/RSJPDHK/RSUI</p>
+            <p class="text-xl font-semibold">Alumni Gathering Event
+            <br>Department of Cardiology and Vascular Medicine FKUI/RSJPDHK/RSUI</p>
+            <h1 class="text-5xl text-fuchsia-600 font-bold">Homecoming Day</h1>
         </div>
         <div class="flex flex-col lg:flex-row justify-between gap-5 ">
             <div class="basis-1/2 order-first lg:order-last lg:relative">
-                <h4 class="font-sans text-fuchsia-500 text-2xl lg:text-3xl font-semibold ">Attention to all
+                <h4 class="font-sans text-fuchsia-500 text-2xl lg:text-5xl font-bold ">Attention to all
                     Alumni</h4>
                 <div class="lg:absolute lg:-top-28 lg:left-16">
                     <div class="">
@@ -19,7 +19,7 @@
             </div>
 
             <div class="basis-1/2 order-last lg:order-first">
-                <h4 class="font-sans text-fuchsia-600 text-2xl lg:text-3xl font-semibold ">Battle of Colors</h4>
+                <h4 class="font-sans text-fuchsia-600 text-2xl lg:text-5xl font-bold ">Battle of Colors</h4>
                 {{-- <div class="badge rounded-2xl py-6 px-8 bg-[#74c5d5] border-none">
                 </div> --}}
                 <div class="flex flex-col items-center">
@@ -27,7 +27,7 @@
                         <div class="flex flex-row justify-around">
                             <div class="items-center justify-center flex gap-1 lg:gap-3 text-white">
                                 <i class="fa-regular fa-calendar-days text-4xl"></i>
-                                <div class="text-sm uppercase font-semibold">
+                                <div class="text-sm uppercase font-semibold lg:text-lg">
                                     <p>October 26<sup>th</sup>, 2024</p>
                                     <p>17.00 - 21.00 WIB</p>
                                 </div>
@@ -41,18 +41,18 @@
                             </div>
                         </div>
                     </div>
+                    <h4 class="uppercase text-center mb-4 font-bold text-lg lg:text-3xl mt-5 text-blue-900">Show your spirit: <br> Dress in your alumni color!</h4>
                     <div class="py-4 w-full lg:w-1/2">
-                        <h4 class="uppercase text-center mb-4 font-semibold text-lg text-blue-900">Show your spirit: <br> Dress in your alumni color!</h4>
                         <div class="card bg-sky-700 bg-opacity-20">
                             <div class="card-body">
-                                <div class="flex flex-col gap-3">
+                                <div class="flex flex-col gap-3 text-lg lg:text-2xl">
                                     <div class="flex flex-row items-center gap-5 text-white font-semibold uppercase">
                                         <i class="fa-solid fa-shirt "></i>
                                         <p>1968 - 1992: white</p>
                                     </div>
                                     <div class="flex flex-row items-center gap-5 text-red-600 font-semibold uppercase">
                                         <i class="fa-solid fa-shirt"></i>
-                                        <p>1993 - 2008: ref</p>
+                                        <p>1993 - 2008: red</p>
                                     </div>
                                     <div class="flex flex-row items-center gap-5 text-green-600 font-semibold uppercase">
                                         <i class="fa-solid fa-shirt"></i>
