0eef1226fc5e4d38f5dd48d9777663e2bef25ed8
Asep Saepuloh <asep.cgeds@gmail.com>
2024-07-22 16:43:13 +0700

    feat: homecoming page

--- resources/views/livewire/pages/homecoming.blade.php
+++ resources/views/livewire/pages/homecoming.blade.php
@@ -1,10 +1,77 @@
 <div class="bg-slate-50">
-    <div class="bg-local pt-20 pb-52 lg:px-20 px-5 bg-blog h-screen">
+    <div class="bg-local pt-20 pb-52 lg:px-20 px-5 bg-home-coming">
         <div class="flex-col flex gap-3 mb-16">
-            <h1 class="text-4xl text-primary-600 font-semibold">Homecoming</h1>
+            <h1 class="text-4xl text-primary-600 font-semibold">Homecoming Day</h1>
+            <p>Alumni Gathering Event</p>
+            <p>Department of Cardiology and Vascular Medicine FKUI/RSJPDHK/RSUI</p>
         </div>
-        <div class="flex flex-col items-center justify-center h-full">
-            <h1 class="text-4xl text-gray-400">Coming soon</h1>
+        <div class="flex flex-col lg:flex-row justify-between gap-5 ">
+            <div class="basis-1/2 order-first lg:order-last lg:relative">
+                <h4 class="font-sans text-fuchsia-500 text-2xl lg:text-3xl font-semibold ">Attention to all
+                    Alumni</h4>
+                <div class="lg:absolute lg:-top-28 lg:left-16">
+                    <div class="">
+                        <img src="images/homecoming-attend.png" class="w-full" alt="">
+                    </div>
+                    <h1 class="uppercase text-3xl lg:text-5xl font-bold text-fuchsia-700">It's time to reunite,
+                        reminisce, and create new memories</h1>
+                </div>
+            </div>
+
+            <div class="basis-1/2 order-last lg:order-first">
+                <h4 class="font-sans text-fuchsia-600 text-2xl lg:text-3xl font-semibold ">Battle of Colors</h4>
+                {{-- <div class="badge rounded-2xl py-6 px-8 bg-[#74c5d5] border-none">
+                </div> --}}
+                <div class="flex flex-col items-center">
+                    <div class="bg-sky-700 rounded-lg py-6 mt-8 w-full">
+                        <div class="flex flex-row justify-around">
+                            <div class="items-center justify-center flex gap-1 lg:gap-3 text-white">
+                                <i class="fa-regular fa-calendar-days text-4xl"></i>
+                                <div class="text-sm uppercase font-semibold">
+                                    <p>October 26<sup>th</sup>, 2024</p>
+                                    <p>17.00 - 21.00 WIB</p>
+                                </div>
+                            </div>
+                            <div class="items-center justify-center flex gap-1 lg:gap-3 text-white">
+                                <i class="fa-solid fa-location-dot text-4xl"></i>
+                                <div class="text-sm uppercase font-semibold">
+                                    <p>JW Marriot</p>
+                                    <p>mutiara ballroom</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="py-4 w-full lg:w-1/2">
+                        <h4 class="uppercase text-center mb-4 font-semibold text-lg text-blue-900">Show your spirit: <br> Dress in your alumni color!</h4>
+                        <div class="card bg-sky-700 bg-opacity-20">
+                            <div class="card-body">
+                                <div class="flex flex-col gap-3">
+                                    <div class="flex flex-row items-center gap-5 text-white font-semibold uppercase">
+                                        <i class="fa-solid fa-shirt "></i>
+                                        <p>1968 - 1992: white</p>
+                                    </div>
+                                    <div class="flex flex-row items-center gap-5 text-red-600 font-semibold uppercase">
+                                        <i class="fa-solid fa-shirt"></i>
+                                        <p>1993 - 2008: ref</p>
+                                    </div>
+                                    <div class="flex flex-row items-center gap-5 text-green-600 font-semibold uppercase">
+                                        <i class="fa-solid fa-shirt"></i>
+                                        <p>2009 - 2017: green</p>
+                                    </div>
+                                    <div class="flex flex-row items-center gap-5 text-blue-600 font-semibold uppercase">
+                                        <i class="fa-solid fa-shirt"></i>
+                                        <p>2018 - now: blue</p>
+                                    </div>
+                                    <div class="flex flex-row items-center gap-5 text-yellow-300 font-semibold uppercase">
+                                        <i class="fa-solid fa-shirt"></i>
+                                        <p>PPDS : yellow</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
