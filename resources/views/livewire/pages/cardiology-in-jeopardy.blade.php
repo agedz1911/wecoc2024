507342cb60be158e54e6e875873bd712074c240f
Asep Saepuloh <asep.cgeds@gmail.com>
2024-07-29 16:32:50 +0700

    feat ws and CIJ

--- resources/views/livewire/pages/cardiology-in-jeopardy.blade.php
+++ resources/views/livewire/pages/cardiology-in-jeopardy.blade.php
@@ -1,9 +1,43 @@
 <div class="bg-slate-50 ">
-    <div class="bg-local pt-20 pb-52 lg:px-20 px-5 bg-blog ">
-        <div class="flex-col flex gap-3 mb-16">
-            <h1 class="text-4xl text-primary-600 font-semibold">Cardiology in Jeopardy</h1>
+    <div class="bg-local pt-20 pb-52 lg:px-20 px-5 bg-cij ">
+        
+        <div class="container mx-auto items-center">
+            <div class="flex flex-col gap-8 justify-between lg:flex-row">
+
+                <div class="w-full px-0 lg:pl-10 lg:py-5 order-last lg:order-first">
+                    <div>
+                        <h4 class="text-3xl lg:text-5xl text-white font-semibold">Prepare your team</h4>
+                        <div class="relative">
+                            <img src="images/cij-piala.png" class="w-60 opacity-85" alt="">
+                            <img src="images/cij-piala.png" class="w-60 animate-ping absolute top-0 left-0" alt="">
+                        </div>
+                        <h4 class="text-3xl lg:text-5xl text-white font-semibold">to earn biggest points!</h4>
+                    </div>
+                    <div class="card bg-base-100 w-full lg:w-96 shadow-xl mt-10">
+                        <figure>
+                            <img src="images/info-cij.jpg" class="rounded-lg" alt="">
+                        </figure>
+                    </div>
+                    <p class="text-slate-100 font-semibold italic text-xl lg:mt-8 mt-4">Register your team and join
+                        the battle of</p>
+                    <h1
+                        class="text-4xl text-center lg:text-left mt-5 text-white mb-5 font-bold uppercase">
+                        Cardiology in jeopardy</h1>
+                    <a href="/cardiology-in-jeopardy" class="btn btn-lg lg:w-1/3 float-end animate-pulse"><i class="fa-regular fa-pen-to-square"></i>Register Now!</a>
+                </div>
+                <div class="w-full justify-center md:pt-10 order-first md:order-last">
+                    <div class="mockup-window border bg-white">
+                        <div class="flex justify-center px-4 py-5 bg-slate-100">
+                            <video class=" rounded-lg" controls muted autoplay loop>
+                                <source src="images/video/cij-promo.mp4" type="video/mp4" />
+                                Your browser does not support the video tag.
+                            </video>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="flex flex-col lg:flex-row justify-stretch gap-10 lg:px-10">
+        {{-- <div class="flex flex-col lg:flex-row justify-stretch gap-10 lg:px-10">
             <div class="order-last md:order-first ">
                 <div class="card bg-base-100 w-full lg:w-96 shadow-xl">
                     <figure>
@@ -21,6 +55,6 @@
                     <li>CIJ registrants must first be registered as participants of the WECOC symposium.</li>
                 </ul>
             </div>
-        </div>
+        </div> --}}
     </div>
 </div>
--- resources/views/livewire/pages/registration.blade.php
+++ resources/views/livewire/pages/registration.blade.php
@@ -142,6 +142,143 @@
                 </div>
             </div>
         </div>
+        <div class="flex-col flex gap-3 my-10">
+            <h1 class="text-4xl text-primary-700 font-semibold">Workshops</h1>
+        </div>
+        <div x-data="{ openTab: 1 }" class="p-2">
+            <div class="w-full mx-auto">
+                <div class="mb-4 flex space-x-4 p-2 bg-white rounded-lg shadow-md">
+                    <button x-on:click="openTab = 1" :class="{ 'bg-primary-600 text-white': openTab === 1 }" class="flex-1 py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-primary transition-all duration-300">25 October 2024</button>
+                    <button x-on:click="openTab = 2" :class="{ 'bg-primary-600 text-white': openTab === 2 }" class="flex-1 py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-primary transition-all duration-300">26 October 2024</button>
+                </div>
+
+                <div x-show="openTab === 1" class="transition-all duration-300 bg-white p-4 rounded-lg shadow-md border-l-4 border-primary-600">
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 ">
+                        <div class="card bg-price ">
+                            <div class="card-body w-full">
+                                <h2 class="card-title text-primary-500">Cardiac Critical Care <div class="badge">WS 1</div></h2>
+                                <div class="border my-2 border-primary-600"></div>
+                                <p><i class="fa-regular fa-circle-xmark mr-3"></i>Non Hands on workshop</p>
+                                <div class="card-actions justify-end">
+                                    {{-- <a href="https://expo.virconex-id.com/registration/wecoc2024/" class="btn btn-primary">Register </a> --}}
+                                    <a href="javascript:void(0)" class="btn btn-primary">Register </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card bg-price ">
+                            <div class="card-body w-full">
+                                <div>
+                                    <h2 class="card-title text-primary-500">Invasive cardiology: Mastering Coronary Physiology Assesment: Beyond Anatomy</h2>
+                                    <div class="badge">WS 2</div>
+                                </div>
+                                <div class="border my-2 border-primary-600"></div>
+                                <p><i class="fa-regular fa-circle-xmark mr-3"></i>Non Hands on workshop</p>
+                                <div class="card-actions justify-end">
+                                    <a href="javascript:void(0)" class="btn btn-primary">Register </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card bg-price ">
+                            <div class="card-body w-full">
+                                <div>
+                                    <h2 class="card-title text-primary-500">Mitral Regurgitation: From Anatomy to Treatment, Focus on Echocardiography</h2>
+                                    <div class="badge">WS 3</div>
+                                </div>
+                                <div class="border my-2 border-primary-600"></div>
+                                <p><i class="fa-regular fa-circle-xmark mr-3"></i>Non Hands on workshop</p>
+                                <div class="card-actions justify-end">
+                                    <a href="javascript:void(0)" class="btn btn-primary">Register </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card bg-price ">
+                            <div class="card-body w-full">
+                                <div>
+                                    <h2 class="card-title text-primary-500">Acute Heart Failure</h2>
+                                    <div class="badge">WS 4</div>
+                                </div>
+                                <div class="border my-2 border-primary-600"></div>
+                                <p><i class="fa-regular fa-circle-xmark mr-3"></i>Non Hands on workshop</p>
+                                <div class="card-actions justify-end">
+                                    <a href="javascript:void(0)" class="btn btn-primary">Register </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div x-show="openTab === 2" class="transition-all duration-300 bg-white p-4 rounded-lg shadow-md border-l-4 border-primary-600">
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 ">
+                        <div class="card bg-price ">
+                            <div class="card-body w-full">
+                                <div>
+                                    <h2 class="card-title text-primary-500">Aritmia - Sudden Cardiac Death</h2>
+                                    <div class="badge">WS 5</div>
+                                </div>
+                                <div class="border my-2 border-primary-600"></div>
+                                <p><i class="fa-regular fa-circle-xmark mr-3"></i>Non Hands on workshop</p>
+                                <div class="card-actions justify-end">
+                                    {{-- <a href="https://expo.virconex-id.com/registration/wecoc2024/" class="btn btn-primary">Register </a> --}}
+                                    <a href="javascript:void(0)" class="btn btn-primary">Register </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card bg-price ">
+                            <div class="card-body w-full">
+                                <div>
+                                    <h2 class="card-title text-primary-500">Imaging in Congenital Heart Disease (Focus in ToF)</h2>
+                                    <div class="badge">WS 6</div>
+                                </div>
+                                <div class="border my-2 border-primary-600"></div>
+                                <p><i class="fa-regular fa-circle-check mr-3"></i>Hands on workshop</p>
+                                <div class="card-actions justify-end">
+                                    <a href="javascript:void(0)" class="btn btn-primary">Register </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card bg-price ">
+                            <div class="card-body w-full">
+                                <div>
+                                    <h2 class="card-title text-primary-500">Imaging --> Cardiovascular CT in Acute Chest Pain Settings</h2>
+                                    <div class="badge">WS 7</div>
+                                </div>
+                                <div class="border my-2 border-primary-600"></div>
+                                <p><i class="fa-regular fa-circle-check mr-3"></i>Hands on workshop</p>
+                                <div class="card-actions justify-end">
+                                    <a href="javascript:void(0)" class="btn btn-primary">Register </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card bg-price ">
+                            <div class="card-body w-full">
+                                <div>
+                                    <h2 class="card-title text-primary-500">An Overview of Cardiopulmonary Excercise Testing in Clinical Practice</h2>
+                                    <div class="badge">WS 8</div>
+                                </div>
+                                <div class="border my-2 border-primary-600"></div>
+                                <p><i class="fa-regular fa-circle-check mr-3"></i>Hands on workshop</p>
+                                <div class="card-actions justify-end">
+                                    <a href="javascript:void(0)" class="btn btn-primary">Register </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card bg-price ">
+                            <div class="card-body w-full">
+                                <div>
+                                    <h2 class="card-title text-primary-500">Vascular --> Mastering Non-invasive Diagnostic Vascular: Duplex-Doppler Ultrasound and Plethysmography in Carotid, Renal, Upper-Lower Extremity, and AV Shunt Haemodialysis Access</h2>
+                                    <div class="badge">WS 9</div>
+                                </div>
+                                <div class="border my-2 border-primary-600"></div>
+                                <p><i class="fa-regular fa-circle-check mr-3"></i>Hands on workshop</p>
+                                <div class="card-actions justify-end">
+                                    <a href="javascript:void(0)" class="btn btn-primary">Register </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <div class="bg-local pt-24 pb-52 lg:px-20 px-5 bg-blog" id="reg-info">
